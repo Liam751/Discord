6c6501ac10714e5afe895ed93c0b372d1b849325
Liam751 <55156691+Liam751@users.noreply.github.com>
2020-02-07 11:28:09 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,79 +2,75 @@ const Discord = require('discord.js');
 //fd
 
 const client = new Discord.Client();
-/*
-var command = "!boy weakness fire";
+
+var command = "!boy fire";
 var arguments = command.split(' ');
-var output="";
+var output = "";
 if (arguments[0] == "!boy") {
     if (arguments[1]) {
-        switch ( arguments [1] ) {
-            case "weakness" :
-                if (arguments[2]) {
-                    switch (arguments[2]){
-                        case "fire" :
-                            output += "Weakness: Ground, Rock, Water";
-                            break;
-                        case "water" :
-                            output += "Weakness: Electric, Grass";
-                            break;
-                        case "grass" :
-                            output += "Weakness: Bug, Fire, Flying, Ice, Poison";
-                            break;
-                        case "flying" :
-                            output += "Weakness: Electric, Ice, Rock";
-                            break;
-                        case "ground" :
-                            output += "Weakness: Grass, Ice, Water";
-                            break;
-                        case "fighting" :
-                            output += "Weakness: Fairy, Flying, Psychic";
-                            break;
-                        case "rock" :
-                            output += "Weakness: Fighting, Grass, Ground, Steel, Water";
-                            break;
-                        case "psychic" :
-                            output += "Weakness: Bug, Dark, Ghost";
-                            break;
-                        case "steel" :
-                            output += "Weakness: Fighting, Fire, Ground";
-                            break;
-                        case "ice" :
-                            output += "Weakness: Fighting, Fire, Rock, Steel";
-                            break;
-                        case "poison" :
-                            output += "Weakness: Ground, Psychic";
-                            break;
-                        case "dark" :
-                            output += "Weakness: Bug, Fairy, Fighting";
-                            break;
-                        case "dragon" :
-                            output += "Weakness: Dragon, Fairy, Ice";
-                            break;
-                        case "electric" :
-                            output += "Weakness: Ground";
-                            break;
-                        case "fairy" :
-                            output += "Weakness: Poison, Steel";
-                            break;
-                        case "ghost" :
-                            output += "Weakness: Dark, Ghost";
-                            break;
-                        case "normal" :
-                            output += "Weakness: Fighting";
-                            break;
-                    }
-                }
-            break;
+        switch (arguments[1]) {
+            case "fire":
+                output = "Weakness: Ground, Rock, Water";
+                break;
+            case "water":
+                output = "Weakness: Electric, Grass";
+                break;
+            case "grass":
+                output = "Weakness: Bug, Fire, Flying, Ice, Poison";
+                break;
+            case "flying":
+                output = "Weakness: Electric, Ice, Rock";
+                break;
+            case "ground":
+                output = "Weakness: Grass, Ice, Water";
+                break;
+            case "fighting":
+                output = "Weakness: Fairy, Flying, Psychic";
+                break;
+            case "rock":
+                output = "Weakness: Fighting, Grass, Ground, Steel, Water";
+                break;
+            case "psychic":
+                output = "Weakness: Bug, Dark, Ghost";
+                break;
+            case "steel":
+                output = "Weakness: Fighting, Fire, Ground";
+                break;
+            case "ice":
+                output = "Weakness: Fighting, Fire, Rock, Steel";
+                break;
+            case "poison":
+                output = "Weakness: Ground, Psychic";
+                break;
+            case "dark":
+                output = "Weakness: Bug, Fairy, Fighting";
+                break;
+            case "dragon":
+                output = "Weakness: Dragon, Fairy, Ice";
+                break;
+            case "electric":
+                output = "Weakness: Ground";
+                break;
+            case "fairy":
+                output = "Weakness: Poison, Steel";
+                break;
+            case "ghost":
+                output = "Weakness: Dark, Ghost";
+                break;
+            case "normal":
+                output = "Weakness: Fighting";
+                break;
         }
-
-        message.channel.send(output)
-
     }
-
+    break;
 }
-*/
+message.channel.send(output)
 
+
+
+
+
+/*
 client.on('ready', () => {
 
     console.log('I am ready!');
@@ -150,7 +146,7 @@ client.on('message', message => {
         message.reply('Weakness: Fighting');
     }
 });
-
+*/
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
